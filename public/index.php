eb3ff6f4942265215d74efb3eecb577e54ea15fa
Oskar Grosser <mail@oskargrosser.com>
2024-12-01 08:55:15 +0100

    Add initial page
    Has placeholder styles

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="/resources/styles/index.css">
+  </head>
+  <body class="page">
+    <header class="page__header"></header>
+    <main class="page__content"></main>
+    <footer class="page__footer"></footer>
+  </body>
+</html>
