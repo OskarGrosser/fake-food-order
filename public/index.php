0b646f1be2fe004e45f9ff2168685e4f73842164
Oskar Grosser <mail@oskargrosser.com>
2024-12-01 09:08:19 +0100

    Add restaurant banner

--- public/index.php
+++ public/index.php
@@ -7,7 +7,17 @@
   </head>
   <body class="page">
     <header class="page__header"></header>
-    <main class="page__content"></main>
+    <main class="page__content">
+      <section class="menu">
+        <header class="menu__header restaurant">
+          <div class="banner">
+            <div class="banner__content menu__content">
+              <img class="restaurant__logo" width="80" height="80" src="https://picsum.photos/id/625/80/80" alt="Restaurant Logo">
+            </div>
+          </div>
+        </header>
+      </section>
+    </main>
     <footer class="page__footer"></footer>
   </body>
 </html>
