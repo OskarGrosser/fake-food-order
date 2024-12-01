ebed778f7e751e15a1dacf071ac36b929c3e6689
Oskar Grosser <mail@oskargrosser.com>
2024-12-01 09:14:34 +0100

    Add restaurant name and rating
    Also, update ALT of logo

--- public/index.php
+++ public/index.php
@@ -12,8 +12,16 @@
         <header class="menu__header restaurant">
           <div class="banner">
             <div class="banner__content menu__content">
-              <img class="restaurant__logo" width="80" height="80" src="https://picsum.photos/id/625/80/80" alt="Restaurant Logo">
+              <img class="restaurant__logo" width="80" height="80" src="https://picsum.photos/id/625/80/80" alt="Tim's Trucker Tables">
             </div>
+          </div>
+          <div class="menu__content collapsed">
+            <h1 class="restaurant__name collapsed__end--block">Tim's Trucker Tables</h1>
+            <p class="restaurant__rating collapsed__start--block rating">
+              <span class="rating__label">Rating:</span>
+              <span class="rating__stars">&#x2605;&#x2605;&#x2605;&#x2606;&#x2606;</span>
+              <span class="rating__numeric">(3/5)</span>
+            </p>
           </div>
         </header>
       </section>
